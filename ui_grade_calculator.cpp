505ae86bd977f4df0ce462fe92f0eb3dccdc9ee5
etonhsu <etonhsu111@gmail.com>
2017-05-24 09:20:17 -0700

    Linked Spin Box with Sliders
    Linked private slots of spin boxes so that when value is changed,
    slider value is changed as well

--- ui_grade_calculator.cpp
+++ ui_grade_calculator.cpp
@@ -19,40 +19,88 @@ void MainWindow::on_hw_slider_1_valueChanged(int value)
     ui->hw_score_1->setValue(value);
 }
 
+void MainWindow::on_hw_score_1_valueChanged(double arg1)
+{
+    ui->hw_slider_1->setValue(arg1);
+}
+
+
 void MainWindow::on_hw_slider_2_valueChanged(int value)
 {
     ui->hw_score_2->setValue(value);
 }
+
+void MainWindow::on_hw_score_2_valueChanged(double arg1)
+{
+    ui->hw_slider_2->setValue(arg1);
+}
+
 
 void MainWindow::on_hw_slider_3_valueChanged(int value)
 {
     ui->hw_score_3->setValue(value);
 }
 
+void MainWindow::on_hw_score_3_valueChanged(double arg1)
+{
+    ui->hw_slider_3->setValue(arg1);
+}
+
+
 void MainWindow::on_hw_slider_4_valueChanged(int value)
 {
     ui->hw_score_4->setValue(value);
 }
+
+void MainWindow::on_hw_score_4_valueChanged(double arg1)
+{
+    ui->hw_slider_4->setValue(arg1);
+}
+
 
 void MainWindow::on_hw_slider_5_valueChanged(int value)
 {
     ui->hw_score_5->setValue(value);
 }
 
+void MainWindow::on_hw_score_5_valueChanged(double arg1)
+{
+    ui->hw_slider_5->setValue(arg1);
+}
+
+
 void MainWindow::on_hw_slider_6_valueChanged(int value)
 {
     ui->hw_score_6->setValue(value);
 }
+
+void MainWindow::on_hw_score_6_valueChanged(double arg1)
+{
+    ui->hw_slider_6->setValue(arg1);
+}
+
 
 void MainWindow::on_hw_slider_7_valueChanged(int value)
 {
     ui->hw_score_7->setValue(value);
 }
 
+void MainWindow::on_hw_score_7_valueChanged(double arg1)
+{
+    ui->hw_slider_7->setValue(arg1);
+}
+
+
 void MainWindow::on_hw_slider_8_valueChanged(int value)
 {
     ui->hw_score_8->setValue(value);
 }
+
+void MainWindow::on_hw_score_8_valueChanged(double arg1)
+{
+    ui->hw_slider_8->setValue(arg1);
+}
+
 
 
 void MainWindow::on_midterm_slider_1_valueChanged(int value)
@@ -60,12 +108,29 @@ void MainWindow::on_midterm_slider_1_valueChanged(int value)
     ui->midterm_score_1->setValue(value);
 }
 
+void MainWindow::on_midterm_score_1_valueChanged(double arg1)
+{
+    ui->midterm_slider_1->setValue(arg1);
+}
+
+
 void MainWindow::on_midterm_slider_2_valueChanged(int value)
 {
     ui->midterm_score_2->setValue(value);
 }
 
+void MainWindow::on_midterm_score_2_valueChanged(double arg1)
+{
+    ui->midterm_slider_2->setValue(arg1);
+}
+
+
 void MainWindow::on_final_slider_valueChanged(int value)
 {
     ui->final_score->setValue(value);
+}
+
+void MainWindow::on_final_score_valueChanged(double arg1)
+{
+    ui->final_slider->setValue(arg1);
 }
--- ui_grade_calculator.h
+++ ui_grade_calculator.h
@@ -26,11 +26,23 @@ private slots:
     void on_hw_slider_7_valueChanged(int value);
     void on_hw_slider_8_valueChanged(int value);
 
+    void on_hw_score_1_valueChanged(double arg1);
+    void on_hw_score_2_valueChanged(double arg1);
+    void on_hw_score_3_valueChanged(double arg1);
+    void on_hw_score_4_valueChanged(double arg1);
+    void on_hw_score_5_valueChanged(double arg1);
+    void on_hw_score_6_valueChanged(double arg1);
+    void on_hw_score_7_valueChanged(double arg1);
+    void on_hw_score_8_valueChanged(double arg1);
+
     void on_midterm_slider_1_valueChanged(int value);
+    void on_midterm_score_1_valueChanged(double arg1);
 
     void on_midterm_slider_2_valueChanged(int value);
+    void on_midterm_score_2_valueChanged(double arg1);
 
     void on_final_slider_valueChanged(int value);
+    void on_final_score_valueChanged(double arg1);
 
 private:
     Ui::MainWindow *ui;
